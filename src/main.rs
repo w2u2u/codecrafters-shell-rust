ab4320debe2d7efe6d9a0687b3f9120937fdac6a
Warunyu Jaimuang <warunyu.pon@gmail.com>
2024-05-25 16:00:02 +0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::process;
+use std::{path::Path, process};
 
 struct ShellBuiltin {
     name: String,
@@ -15,6 +15,22 @@ impl ShellBuiltin {
 
     fn is_shell_builtin(&self) -> bool {
         matches!(self.name.as_str(), "echo" | "type" | "exit")
+    }
+
+    fn get_path(&self) -> Option<String> {
+        let path_env = std::env::var("PATH").unwrap_or_default();
+        let paths = path_env.split(':').collect::<Vec<&str>>();
+
+        paths.iter().find_map(|&path| {
+            let mut full_path = Path::new(path).join(&self.name);
+            full_path.set_extension("");
+
+            if full_path.exists() {
+                Some(full_path.to_string_lossy().to_string())
+            } else {
+                None
+            }
+        })
     }
 }
 
@@ -44,7 +60,13 @@ impl Command {
             Command::Type(cmd) if cmd.is_shell_builtin() => {
                 println!("{} is a shell builtin", cmd.name)
             }
-            Command::Type(cmd) => println!("{} not found", cmd.name),
+            Command::Type(cmd) => {
+                if let Some(path) = cmd.get_path() {
+                    println!("{} is {}", cmd.name, path);
+                } else {
+                    println!("{} not found", cmd.name);
+                }
+            }
             Command::Exit(code) => process::exit(*code),
             Command::Unknown(cmd) => println!("{}: command not found", cmd),
         }
