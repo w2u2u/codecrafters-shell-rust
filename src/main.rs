9dcd36a5c5e7cfdc9c890fcd605371d60e6680b8
Warunyu Jaimuang <warunyu.pon@gmail.com>
2024-05-25 12:05:11 +0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,7 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+
+    // Command not found
+    println!("{}: command not found", input.trim());
 }
